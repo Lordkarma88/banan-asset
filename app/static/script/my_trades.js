608241bddc5af8e1100870c133a68de520a4ede8
Lordkarma88 <calvin.hg.lamb@yandex.com>
2023-05-06 15:25:40 -0500

    made first select2 form for fav assets

--- app/static/script/my_trades.js
+++ app/static/script/my_trades.js
@@ -1,6 +1,8 @@
-$("#state").select2({
-  placeholder: "state",
-  allowClear: true,
-  closeOnSelect: false,
-  width: "style",
+$(document).ready(() => {
+  $("#state").select2({
+    placeholder: "state",
+    allowClear: true,
+    width: "style",
+  });
+  $("#select2-state-container").siblings()[0].lastChild.id = "states";
 });
